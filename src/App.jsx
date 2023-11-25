c5315188c77c7c198f40c7de27bbfa2aea3ee182
AndriuszzZ <andre.genolima@gmail.com>
2023-11-24 21:36:33 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Andre Lima" 
         content="Texto" 

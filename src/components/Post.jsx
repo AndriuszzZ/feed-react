39f97ac0f5503664ce3ca29c5b28f830225e2e70
AndriuszzZ <andre.genolima@gmail.com>
2023-11-26 21:41:04 -0300

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,7 +11,33 @@ export function Post() {
                         <span>Web Developer</span>
                     </div>
                 </div>
+
+                <time title="20 de novembro de 2023" dateTime="2023-11-20 12:12:02">Publicado há 1h</time>
             </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p><a href="">jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href=""> #nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder="Deixe um comentário"
+                />
+
+                <button type="sublit"></button>
+            </form>
         </article>
     )
 }
